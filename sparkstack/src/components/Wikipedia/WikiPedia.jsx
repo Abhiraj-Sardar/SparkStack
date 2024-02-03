2f299758787ea472bdcce384bc55d4ff290218c6
Abhiraj <abhirajsardar2003@gmail.com>
2024-02-03 22:26:06 +0530

    update wikipedia

--- sparkstack/src/components/Wikipedia/WikiPedia.jsx
+++ sparkstack/src/components/Wikipedia/WikiPedia.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./WikiPedia.css"
 import Search from './Search'
 import './Search.css'
+
 const WikiPedia = () => {
-    var Theme = () => {
-        var mainElement = document.querySelector(".main");
-        mainElement.style.backgroundColor = "#eee";            
-    };
+    const [counter,setCounter]=useState(1);
     return (
         <div className="container-fluid main">
             <div className="theme-changer">
-                <div className="theme" onClick={Theme()}></div>
+                <div className="theme" onClick={()=>{
+                    var main=document.querySelector(".main");
+                    
+                    if(counter%2!=0)
+                    {
+                        main.style.backgroundColor="#333";
+                        document.querySelector(".searchbar").style.color="#fff";
+                        setCounter(counter+1)
+                    }
+                    else{
+                        main.style.backgroundColor="#F8F4EC"
+                        document.querySelector(".searchbar").style.color="#333";
+                        setCounter(counter+1)
+                    }
+                }}></div>
             </div>
             <div className="container">
                 <div className="row">
