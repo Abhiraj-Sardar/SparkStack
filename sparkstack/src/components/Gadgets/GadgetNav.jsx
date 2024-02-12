16f2bf575e01fb20636386525280359fbe3aa5b0
Abhiraj <abhirajsardar2003@gmail.com>
2024-02-12 17:01:24 +0530

    update all

---
+++ sparkstack/src/components/Gadgets/GadgetNav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./GadgetNav.css"
+const GadgetNav = () => {
+    return (
+            <div className="gadget-navbar">
+                <button className='gadget-btn btn btn-secondary'>MOBILES</button>
+                <button className='gadget-btn btn btn-secondary'>PC</button>
+                <button className='gadget-btn btn btn-secondary'>LAPTOP</button>
+                <button className='gadget-btn btn btn-secondary'>AUDIO</button>
+                <button className='gadget-btn btn btn-secondary'>TABLETS</button>
+                <button className='gadget-btn btn btn-secondary'>WEARABLES</button>
+            </div>
+    )
+}
+
+export default GadgetNav
--- sparkstack/src/components/Gadgets/MobileCard.jsx
+++ sparkstack/src/components/Gadgets/MobileCard.jsx
@@ -1,88 +1,113 @@
 import React from "react";
 import './MobileCard.css';
 import Header from "../Header/Header";
+import GadgetNav from "./GadgetNav";
 
 const MobileCard = () => {
     return (
         <>
         <Header/>
-            <div class="container">
+            <div className="row">
+                <div className="col-2">
 
-                <div class="row pt-5">
-                    <div class="col-md-4 mb-4">
-                        <div class="card">
-                            <img class="card-img-top " src="https://images.unsplash.com/photo-1681907624035-63bf2107301c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjF8MTA3NTg1Nnx8ZW58MHx8fHx8" alt="Card image cap" />
-                            <div class="card-body">
-                                <h5 class="card-title">Card 1</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-danger">Go somewhere</a>
-                            </div>
+                </div>
+                <div className="col-8 mt-2 mb-2">
+                    <GadgetNav/>
+                </div>
+                <div className="col-2">
+
+                </div>
+            </div>
+           
+            <div className="card-container row">
+                <div className="left-side col-2">
+                    <h4>TRENDING</h4>
+                    <div className="image">
+                        <img src="https://images.unsplash.com/photo-1598327105666-5b89351aff97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlc3xlbnwwfHwwfHx8MA%3D%3D" alt="" srcset="" />
+                        <div className="image-data">
+                            Xiaomi 14 Ultra Launch Date Tipped, Camera Specifications Surface Online
                         </div>
                     </div>
-
-                    {/* <!-- Repeat the above card structure for the next two cards in the same row --> */}
-
-                    {/* <!-- Second Card --> */}
-                    <div class="col-md-4 mb-4">
-                        <div class="card">
-                            <img class="card-img-top" src="https://images.unsplash.com/photo-1542051841857-5f90071e7989?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8amFwYW58ZW58MHx8MHx8fDA%3D" alt="Card image cap" />
-                            <div class="card-body">
-                                <h5 class="card-title">Card 2</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-danger">Go somewhere</a>
-                            </div>
+                    <div className="image">
+                        <img src="https://images.unsplash.com/photo-1598327105666-5b89351aff97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlc3xlbnwwfHwwfHx8MA%3D%3D" alt="" srcset="" />
+                        <div className="image-data">
+                            Xiaomi 14 Ultra Launch Date Tipped, Camera Specifications Surface Online
                         </div>
                     </div>
-
-                    {/* <!-- Third Card --> */}
-                    <div class="col-md-4 mb-4">
-                        <div class="card">
-                            <img class="card-img-top" src="https://plus.unsplash.com/premium_photo-1661371927364-e3aec9079c66?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8amFwYW58ZW58MHx8MHx8fDA%3D" alt="Card image cap" />
-                            <div class="card-body">
-                                <h5 class="card-title">Card 3</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-danger">Go somewhere</a>
-                            </div>
+                    <div className="image">
+                        <img src="https://images.unsplash.com/photo-1598327105666-5b89351aff97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlc3xlbnwwfHwwfHx8MA%3D%3D" alt="" srcset="" />
+                        <div className="image-data">
+                            Xiaomi 14 Ultra Launch Date Tipped, Camera Specifications Surface Online
                         </div>
                     </div>
+                    
                 </div>
 
-                <div class="row pt-5">
-                    <div class="col-md-4 mb-4">
-                        <div class="card">
-                            <img class="card-img-top" src="https://images.unsplash.com/photo-1526481280693-3bfa7568e0f3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGphcGFufGVufDB8fDB8fHww" alt="Card image cap" />
-                            <div class="card-body">
-                                <h5 class="card-title">Card 4</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-danger">Go somewhere</a>
-                            </div>
+
+                <div className="center-side col-8">
+                    <h1>Mobile News</h1>
+                    <hr/>
+                    <br />
+                    <div className="gadget-card">
+                        <div className="gadget-image">
+                            <img src="https://images.unsplash.com/photo-1589492477829-5e65395b66cc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW9iaWxlc3xlbnwwfHwwfHx8MA%3D%3D" height={144} alt="" />
+                        </div>
+                        <div className="gadget-desc">
+                            <h4>Vivo V30 Pro With MediaTek Dimensity 8200 SoC, 12GB RAM Allegedly Spotted on Geekbench</h4>
+                            <p>Written by Nithya P Nair, 12 February 2024</p>
+                            <h5>MOBILES</h5>
                         </div>
                     </div>
-
-                    {/* <!-- Repeat the above card structure for the next two cards in the same row --> */}
-
-                    {/* <!-- Second Card --> */}
-                    <div class="col-md-4 mb-4">
-                        <div class="card">
-                            <img class="card-img-top" src="https://images.unsplash.com/photo-1480796927426-f609979314bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGphcGFufGVufDB8fDB8fHww" alt="Card image cap" />
-                            <div class="card-body">
-                                <h5 class="card-title">Card 5</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-danger">Go somewhere</a>
-                            </div>
+                    <br />
+                    <hr/>
+                    <br />
+                    <div className="gadget-card">
+                        <div className="gadget-image">
+                            <img src="https://images.unsplash.com/photo-1589492477829-5e65395b66cc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW9iaWxlc3xlbnwwfHwwfHx8MA%3D%3D" height={144} alt="" />
+                        </div>
+                        <div className="gadget-desc">
+                            <h4>Vivo V30 Pro With MediaTek Dimensity 8200 SoC, 12GB RAM Allegedly Spotted on Geekbench</h4>
+                            <p>Written by Nithya P Nair, 12 February 2024</p>
+                            <h5>MOBILES</h5>
                         </div>
                     </div>
+                    <br />
+                    <hr/>
+                    <br />
+                    <div className="gadget-card">
+                        <div className="gadget-image">
+                            <img src="https://i.gadgets360cdn.com/large/xiaomi_14_pro_2_1707715215339.jpg?downsize=160:120&output-quality=80&output-format=webp" height={144} alt="" />
+                        </div>
+                        <div className="gadget-desc">
+                            <h4>	
+Xiaomi 14 Ultra to Come in a Titanium Special Edition, to Offer Up to 1TB Storage: Report</h4>
+                            <p>Written by Nithya P Nair, 12 February 2024</p>
+                            <h5>MOBILES</h5>
+                        </div>
+                    </div>
+                    <br />
+                    <hr/>
+                </div>
 
-                    {/* <!-- Third Card --> */}
-                    <div class="col-md-4 mb-4">
-                        <div class="card">
-                            <img class="card-img-top" src="https://images.unsplash.com/photo-1547500135-9f6e5a9a6aff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwxMDc1ODU2fHxlbnwwfHx8fHw%3D" alt="Card image cap" />
-                            <div class="card-body">
-                                <h5 class="card-title">Card 6</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                
-                                <a href="#" class="btn btn-danger">Go somewhere</a>
-                            </div>
+
+                <div className="right-side col-2">
+                <h4>TRENDING</h4>
+                    <div className="image">
+                        <img src="https://images.unsplash.com/photo-1598327105666-5b89351aff97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlc3xlbnwwfHwwfHx8MA%3D%3D" alt="" srcset="" />
+                        <div className="image-data">
+                            Xiaomi 14 Ultra Launch Date Tipped, Camera Specifications Surface Online
+                        </div>
+                    </div>
+                    <div className="image">
+                        <img src="https://images.unsplash.com/photo-1598327105666-5b89351aff97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlc3xlbnwwfHwwfHx8MA%3D%3D" alt="" srcset="" />
+                        <div className="image-data">
+                            Xiaomi 14 Ultra Launch Date Tipped, Camera Specifications Surface Online
+                        </div>
+                    </div>
+                    <div className="image">
+                        <img src="https://images.unsplash.com/photo-1598327105666-5b89351aff97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlc3xlbnwwfHwwfHx8MA%3D%3D" alt="" srcset="" />
+                        <div className="image-data">
+                            Xiaomi 14 Ultra Launch Date Tipped, Camera Specifications Surface Online
                         </div>
                     </div>
                 </div>
